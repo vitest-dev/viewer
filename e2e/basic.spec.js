f0896eaa1f1d7d3b3b0ed0470d29da4c5c0ae97e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-31 10:35:23 +0900

    test: tweak

--- e2e/basic.spec.js
+++ e2e/basic.spec.js
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("drop zone and example link visible on load", async ({ page }) => {
+test("basic", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByText("Service worker ready.")).toBeVisible();
   await page.locator('input[type="file"]').setInputFiles("docs/assets/vitest-html-reporter.zip");
